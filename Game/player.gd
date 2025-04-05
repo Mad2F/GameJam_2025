9f8a2c030b0e874ae1a0afb0cb9df6f4c185074b
Madeline <mad2fleury@gmail.com>
2025-04-05 13:58:38 +0200

    merge nanana

--- Game/player.gd
+++ Game/player.gd
@@ -11,6 +11,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -1000.0
 
 var isOnRope = false
+var timeoff0 = 10
+var timeoff = 0
 
 # Start front idle animation on load
 func _ready():
@@ -19,33 +21,55 @@ func _ready():
 
 
 func _physics_process(delta):
-	var movement = "Idle"
+	var movement = "Walk_Idle"
 	velocity = Vector2.ZERO
 	var falling = false
 	
-	if not is_on_floor() and not isOnRope:
-		velocity.y += GRAVITY
-		falling = true
+	#Small timeoff for playability
+	if (timeoff > 0):
+		timeoff = timeoff - 1
+		return
 	
-	# Handle Jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = move_toward(JUMP_VELOCITY, 0, SPEED)
+	#Player is available
+	#If not on rope :
+	
+	if not isOnRope:
+		#jump on/off rope
+		if Input.is_action_pressed("climb"):
+			print("CLIMB ON")
+			isOnRope = true
+			timeoff = timeoff0
+			return
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-		movement = "Walk"
+		#add gravity
+		if not is_on_floor():
+			velocity.y += GRAVITY
+			falling = true
+	
+		# Handle Jump.
+		if Input.is_action_just_pressed("move_jump") and is_on_floor():
+			velocity.y = move_toward(JUMP_VELOCITY, 0, SPEED)
+			print("JUMP")
+
+		# Get the input direction and handle the movement/deceleration.
+		var direction = Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+			movement = "Walk"
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	#rope movements:
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	var updown = Input.get_axis("ui_up", "ui_down")
-	if not isOnRope and Input.is_action_just_pressed("climb"):
-		isOnRope = true
-	if isOnRope and Input.is_action_just_pressed("climb"):
-		isOnRope = false
-	if isOnRope:
+		movement = "Climb_Idle"
+		
+		if Input.is_action_pressed("climb"):
+			print("CLIMB OFF")
+			isOnRope = false
+			timeoff = timeoff0
+			return
+			
+		var updown = Input.get_axis("ui_up", "ui_down")
 		if updown:
 			velocity.y = updown * SPEED
 			movement = "Climb"
