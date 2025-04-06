9766f4e044213e2116a15bee5737efff3647d545
Civ92 <166902557+Civ92@users.noreply.github.com>
2025-04-06 11:10:14 +0200

    Ajout des lucioles

---
+++ Game/Objects/Luciole.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var _maxDisplacement : Vector2 = Vector2(50, 50)
+@export var speed : float = 10
+@export var moving : bool = true
+var _rng = RandomNumberGenerator.new()
+
+var _initial_position : Vector2
+var _displacement : Vector2
+
+func _ready():
+	_initial_position = position
+
+func _process(delta):
+	_displacement.x = clamp(_displacement.x + _rng.randf_range(-speed, speed) * delta, -_maxDisplacement.x, _maxDisplacement.x)
+	_displacement.y = clamp(_displacement.y + _rng.randf_range(-speed, speed) * delta, -_maxDisplacement.y, _maxDisplacement.y)
+	position = _initial_position + _displacement
