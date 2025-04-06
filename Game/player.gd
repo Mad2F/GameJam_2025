1426b65267c0482d47970dc9bdce67dffb1ed4bd
Madeline <mad2fleury@gmail.com>
2025-04-06 14:03:33 +0200

    commit la lanterne

--- Game/player.gd
+++ Game/player.gd
@@ -76,6 +76,15 @@ func _physics_process(delta):
 		#add gravity
 		if not is_on_floor():
 			velocity.y += _get_gravity() * delta
+		
+		if (is_on_floor()):
+			if (Input.is_action_pressed("use_lantern")):
+				$lanterne.visible = true
+				return
+			else:
+				if ($lanterne.visible):
+					$lanterne.visible = false
+
 	
 		# Handle Jump.
 		if Input.is_action_just_pressed("move_jump") and is_on_floor():
@@ -120,7 +129,6 @@ func _physics_process(delta):
 				timeoff = 0
 				print("No rope")
 			return
-
 	#rope movements:
 	else:
 		velocity.y = 0
