96059efd55e441eaaa65ca943fff8e0de0be7bd7
LeoPostic <leo.posticblu@orange.fr>
2025-04-06 09:31:56 +0200

    Ajustable scouting are

--- Game/Objects/LightSource.gd
+++ Game/Objects/LightSource.gd
@@ -3,7 +3,10 @@ extends Node2D
 @export var radius: float = 100.0
 @export var flicker: float = 0.0
 @export var offset: Vector2 = Vector2(0.0, 0.0)
+@export var intensity: float = 1.0
 
+const LightTexture = preload("res://Game/resources/misc/light.png")
+var lightImage = LightTexture.get_image()
 var level_shadow: LevelShadow 
 var level_fog: Fog
 var last_position: Vector2 
@@ -12,6 +15,8 @@ func _ready() -> void:
 	last_position = get_parent().position
 	level_shadow = get_tree().get_root().get_node(get_tree().current_scene.name +"/LevelShadow")
 	level_fog = get_tree().get_root().get_node(get_tree().current_scene.name +"/Fog")	
+	lightImage.resize(lightImage.get_width() * intensity, lightImage.get_height() * intensity,Image.INTERPOLATE_BILINEAR)
+	lightImage.convert(Image.FORMAT_RGBAH)
 	if(level_shadow):
 		level_shadow.add_light_source(get_parent().name, get_parent().position + offset, radius, 1.0, flicker)
 
@@ -20,8 +25,9 @@ func _exit_tree() -> void:
 		level_shadow.remove_light_source(get_parent().name)
 
 func _process(delta: float) -> void:
-	if(level_shadow && get_parent().position != last_position):
-		level_shadow.set_light_position(get_parent().name, get_parent().position + offset)
-	if(level_fog):
-		level_fog.clear_fog(get_parent().position + offset)
+	if(get_parent().position != last_position):
+		if(level_shadow):
+			level_shadow.set_light_position(get_parent().name, get_parent().position + offset)
+		if(level_fog):
+			level_fog.clear_fog(get_parent().position + offset, lightImage)
 	last_position = get_parent().position
