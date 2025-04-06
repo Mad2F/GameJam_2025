c4a9607b86e28513573f5cf159ff0cc6187d5d93
Civ92 <166902557+Civ92@users.noreply.github.com>
2025-04-06 20:45:30 +0200

    Ajout retour au menu depuis la scene Victory

---
+++ Game/Levels/VictoryScreen.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+func _process(delta):
+	if Input.is_action_pressed("enter") || Input.is_action_pressed("escape") || Input.is_action_pressed("move_jump"):
+		get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
