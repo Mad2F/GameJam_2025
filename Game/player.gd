5493d9351191ff19cd52901bdb30e4ddd2310fed
Fleury Madeline <fleury@ingenuity.io>
2025-04-07 10:42:48 +0200

    deplacement de la lanterne pour voir devant soi

--- Game/player.gd
+++ Game/player.gd
@@ -90,11 +90,16 @@ func _physics_process(delta):
 		
 		if (is_on_floor()):
 			if (Input.is_action_pressed("use_lantern")):
-				$lanterne.visible = true
+				if (toRight):
+					$lanterne_droite.visible = true
+				else:
+					$lanterne_gauche.visible = true
 				return
 			else:
-				if ($lanterne.visible):
-					$lanterne.visible = false
+				if ($lanterne_droite.visible):
+					$lanterne_droite.visible = false
+				if ($lanterne_gauche.visible):
+					$lanterne_gauche.visible = false
 	
 		# Handle Jump.
 		if Input.is_action_just_pressed("move_jump") and is_on_floor():
