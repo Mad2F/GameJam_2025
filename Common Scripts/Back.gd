265b5375441d4c02d461d676881f31f690637814
Fleury Madeline <fleury@ingenuity.io>
2025-04-07 13:28:33 +0200

    font integration

--- Common Scripts/Back.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-func _ready():
- 	#connect the pressed signal to a function
-	pressed.connect(_on_button_pressed)
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
--- Common Scripts/panel.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	if Input.is_action_pressed("enter") || Input.is_action_pressed("escape"):
-		get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
